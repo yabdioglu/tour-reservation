4b5a5d1b0871dabaaf01db51425494b0b98890a7
Yıldıray Abdioğlu <yabdioglu37@gmail.com>
2022-09-24 19:54:27 +0300

    00-spring-initialize

---
+++ backend/src/main/java/com/yabdioglu/tourreservation/TourReservationApplication.java
@@ -0,0 +1,13 @@
+package com.yabdioglu.tourreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourReservationApplication.class, args);
+	}
+
+}
