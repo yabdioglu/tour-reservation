3763ac1afb9c70c466543fe3e9b772af92b07b51
yabdioglu <yabdioglu37@gmail.com>
2022-10-02 13:53:21 +0300

    31-username-validation

--- src/main/java/com/yabdioglu/tourreservation/user/vm/UserRequest.java
+++ src/main/java/com/yabdioglu/tourreservation/user/vm/UserRequest.java
@@ -1,10 +1,15 @@
 package com.yabdioglu.tourreservation.user.vm;
 
+import com.yabdioglu.tourreservation.user.UniqueUsername;
 import lombok.Data;
+
+import javax.validation.constraints.Pattern;
 
 @Data
 public class UserRequest {
 
+    @UniqueUsername
+    @Pattern(regexp = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$", message = "must be a well-formed email address")
     private String username;
 
     private String firstName;
