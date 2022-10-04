20fcbd4822f8681a7b7ee2a272f47d98af853aa8
yabdioglu <yabdioglu37@gmail.com>
2022-10-04 21:43:38 +0300

    36-param-title

--- src/main/java/com/yabdioglu/tourreservation/tour/TourController.java
+++ src/main/java/com/yabdioglu/tourreservation/tour/TourController.java
@@ -5,6 +5,9 @@ import com.yabdioglu.tourreservation.tour.vm.TourRequest;
 import com.yabdioglu.tourreservation.tour.vm.TourResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -35,8 +38,9 @@ public class TourController {
         return new TourResponse(tourService.getById(id));
     }
 
-    @GetMapping("/tours/findByTitleContaining/{title}")
-    public Page<TourResponse> findByTitleContaining(@PathVariable String title, Pageable pageable) {
+    @GetMapping("/tours/findByTitleContaining")
+    public Page<TourResponse> findByTitleContaining(@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                                                    @RequestParam(name = "title") String title) {
         return tourService.findByTitleContaining(title,pageable).map(TourResponse::new);
     }
 
